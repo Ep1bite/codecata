6f60da771930bbaf0bee76b945bab37e9eba9b36
Evening_primrose <92074010+Ep1bite@users.noreply.github.com>
2025-05-28 20:05:28 +0900

    Create 07 두 수의 나눗.cpp

---
+++ 07 두 수의 나눗.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    answer = (int) num1 / (double) num2 * 1000;
+    
+    return answer;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
