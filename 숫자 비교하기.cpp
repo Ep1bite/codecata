42395ce25dbee95525cd574ce8644335cff0b462
Evening_primrose <92074010+Ep1bite@users.noreply.github.com>
2025-05-28 19:45:32 +0900

    Create 숫자 비교하기.cpp

---
+++ 숫자 비교하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    
+    if(num1 == num2)
+        return 1;
+    else 
+        return -1;
+    
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
