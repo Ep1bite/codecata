62dda7025a1b2e46f0b0849492685a572cae15fc
Evening_primrose <92074010+Ep1bite@users.noreply.github.com>
2025-05-29 09:26:31 +0900

    Create 11 짝수와 홀수.cpp

---
+++ 11 짝수와 홀.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    if (num % 2 == 0) {
+        return "Even";
+    } else 
+        return "Odd";
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12937
