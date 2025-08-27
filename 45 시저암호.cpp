42113b0cbf57ccdc2b8c288559ecc78e977718c9
Evening_primrose <92074010+Ep1bite@users.noreply.github.com>
2025-08-27 21:02:08 +0900

    Create 45 시저암호.cpp

---
+++ 45 시저암호.cpp
@@ -0,0 +1,18 @@
+#include <string>
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    for (char c : s) {
+        if (c >= 'A' && c <= 'Z') {
+            answer += (c - 'A' + n) % 26 + 'A';
+        } else if (c >= 'a' && c <= 'z') {
+            answer += (c - 'a' + n) % 26 + 'a';
+        } else {
+            answer += c;
+        }
+    }
+    return answer;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12926
