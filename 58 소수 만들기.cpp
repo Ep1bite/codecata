8e6a36c6de613d692c47af0fa14cf5edf061915f
Evening_primrose <92074010+Ep1bite@users.noreply.github.com>
2025-09-22 21:07:42 +0900

    Create 58 소수 만들기.cpp

---
+++ 58 소수 만들기.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+
+    int n = nums.size();
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (isPrime(sum)) {
+                    answer++;
+                }
+            }
+        }
+    }
+
+    return answer;
+}
